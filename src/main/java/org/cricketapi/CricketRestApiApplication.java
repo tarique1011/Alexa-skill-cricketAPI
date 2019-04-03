ddd32663497edb4b328419b00e27678b1e4dc14f
Tarique Javaid <tarique.javaid11@gmail.com>
2019-04-03 08:25:52 +0530

    Project created

---
+++ src/main/java/org/cricketapi/CricketRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.cricketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cricketapi/CricketRestApiApplicationTests.java
@@ -0,0 +1,16 @@
+package org.cricketapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CricketRestApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
